712f87cec8a0ef88a6d41b88ced8025ce1b9d8a8
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-09-06 22:01:14 +0200

    Fix some issues

--- src/StructDicts.jl
+++ src/StructDicts.jl
@@ -4,11 +4,11 @@ using Unrolled
 
 export StructDict
 
-struct StructDict{I,C}
-	del::Base.RefValue{Bool}
-	nextlastid::Base.RefValue{I}
-	id_to_index::Dict{I, Int}
-	components::C
+mutable struct StructDict{I,C}
+	const del::Base.RefValue{Bool}
+	const nextlastid::Base.RefValue{I}
+	const id_to_index::Dict{I, Int}
+	const components::C
     StructDict(components::NamedTuple) = StructDict{UInt64}(components)
 	function StructDict{I}(components::NamedTuple) where {I<:Union{Signed,Unsigned}}
 		allequal(length.(values(components))) || error("All components must have equal length.")
@@ -54,13 +54,20 @@ function Base.push!(sdict::StructDict, t::NamedTuple)
     return sdict
 end
 
-struct Keys{I}
+struct Keys{T,I}
+    t::T
 	ID::I
+end
+function Base.keys(sdict::StructDict)
+    ID = getfield(getfield(sdict, :components), :ID)
+    return Keys(eltype(ID), ID)
 end
 Base.iterate(k::Keys) = Base.iterate(k.ID)
 Base.iterate(k::Keys, state) = Base.iterate(k.ID, state)
+Base.IteratorSize(::Keys) = Base.HasLength()
+Base.length(k::Keys) = length(k.ID)
+Base.eltype(::Keys{T}) where T = T
 
-Base.keys(sdict::StructDict) = Keys(getfield(getfield(sdict, :components), :ID))
 lastkey(sdict::StructDict) = getfield(sdict, :nextlastid)[]
 
 struct Struct{I, S}
@@ -68,27 +75,47 @@ struct Struct{I, S}
     sdict::S
 end
 
-function Base.getindex(sdict::StructDict, id::Union{Signed,Unsigned}) = Struct(id, sdict)
-
-function Base.getproperty(a::Struct, name::Symbol)
-    id, sdict = getfield(a, :id), getfield(a, :sdict)
+@inline function Base.getindex(sdict::StructDict, id::Union{Signed,Unsigned})
     comps, id_to_index = getfield(sdict, :components), getfield(sdict, :id_to_index)
-    i = get(id_to_index, id, id % Int)
-    return getfield(comps, name)[i]
+    del = getfield(sdict, :del)[]
+    del && !(id in keys(id_to_index)) && error("No agent with the specified id")
+    !del && checkbounds(getfield(comps, :ID), id%Int)
+    return Struct(id, sdict)
 end
 
-function Base.setproperty!(a::Struct, name::Symbol, x)
+@inline function Base.getproperty(a::Struct, name::Symbol)
     id, sdict = getfield(a, :id), getfield(a, :sdict)
     comps, id_to_index = getfield(sdict, :components), getfield(sdict, :id_to_index)
-    i = get(id_to_index, id, id % Int)
-    return (getfield(comps, name)[i] = x)
+    del = getfield(sdict, :del)[]
+    if del
+        i = id_to_index[id]
+        return @inbounds getfield(comps, name)[i]
+    else
+        i = id % Int
+        return getfield(comps, name)[i]
+    end
+end
+
+@inline function Base.setproperty!(a::Struct, name::Symbol, x)
+    id, sdict = getfield(a, :id), getfield(a, :sdict)
+    comps, id_to_index = getfield(sdict, :components), getfield(sdict, :id_to_index)
+    del = getfield(sdict, :del)[]
+    if del
+        i = id_to_index[id]
+        return (@inbounds getfield(comps, name)[i] = x)
+    else
+        i = id % Int
+        return (getfield(comps, name)[i] = x)
+    end
 end
 
 function getfields(a::Struct)
     id, sdict = getfield(a, :id), getfield(a, :sdict)
     comps, id_to_index = getfield(sdict, :components), getfield(sdict, :id_to_index)
-    i = get(id_to_index, id, id % Int)
-    getindexi = ar -> ar[i]
+    del = getfield(sdict, :del)[]
+    i = del ? id_to_index[id] : id % Int
+    checkbounds(getfield(comps, :ID), i)
+    getindexi = ar -> @inbounds ar[i]
     vals = unrolled_map(getindexi, values(comps))
     names = fieldnames(typeof(comps))
     return NamedTuple{names}(vals)
@@ -100,7 +127,7 @@ function Base.show(io::IO, ::MIME"text/plain", x::Struct)
     id, sdict = getfield(x, :id), getfield(x, :sdict)
     comps, id_to_index = getfield(sdict, :components), getfield(sdict, :id_to_index)
     i = get(id_to_index, id, id % Int)
-    fields = NamedTuple(y => getfield(comps, y)[i] for y in fieldnames(comps))
+    fields = NamedTuple(y => getfield(comps, y)[i] for y in fieldnames(typeof(comps)))
     return print(io, "Struct$fields")
 end
 
