c1f1770fff8be85ff54b6c0f9e3cfb508f14654b
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2025-09-11 17:59:21 -0400

    Refactor for new implementations (#8)

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,54 +11,5 @@ using Aqua, Test
         end
     end
 
-    @testset "construction" begin
-        s = IndexedStructVector((x = [10, 20, 30], y = ["a", "b", "c"]))
-        @test length(collect(keys(s))) == 3
-        @test IndexedStructVectors.lastkey(s) == 3
-        @test_throws ErrorException IndexedStructVector((x = [1,2], y = ["a"]))
-    end
-
-    @testset "getindex/getproperty/setproperty!/getfields" begin
-        s = IndexedStructVector((num = [1,2,3], name = ["x","y","z"]))
-        a = s[2]
-
-        @test typeof(a) <: IndexedStructVectors.IndexedView
-        @test a.num == 2
-        @test a.name == "y"
-
-        a.num = 42
-        @test s[2].num == a.num == 42
-
-        nt = getfields(s[2])
-        @test nt.num == 42
-        @test nt.name == "y"
-        @test length(nt) == 2
-    end
-
-    @testset "deleteat!/delete!/push!" begin
-        s = IndexedStructVector((num = [10,20,30,40], tag = ['a','b','c','d']))
-
-        ids_before = collect(keys(s))
-        @test ids_before == [1,2,3,4]
-
-        deleteat!(s, 2)
-        ids_after = collect(keys(s))
-        @test length(ids_after) == 3
-        @test (2 in ids_after) == false
-
-        push!(s, (num = 111, tag = 'z'))
-        new_id = IndexedStructVectors.lastkey(s)
-        @test new_id == s[new_id].ID == id(s[new_id]) == 5
-        @test new_id in collect(keys(s))
-        @test s[new_id].num == 111
-
-        delete!(s, 4)
-        ids_after = collect(keys(s))
-        @test (4 in ids_after) == false
-        @test s.ID[2] == 5
-        @test s[s.ID[2]].num == 111
-        @test length(ids_after) == 3
-
-        @test_throws KeyError delete!(s, 9999)
-    end
+    include("test-dict.jl")
 end
